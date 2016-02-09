6e446b2740c495ccabb4d15249c1399b391697d0
Timothy King <tmk@lordzork.com>
2016-02-09 10:42:44 -0500

    Fixes indentation

--- app/controllers/nfg_csv_importer/imports_controller.rb
+++ app/controllers/nfg_csv_importer/imports_controller.rb
@@ -1,8 +1,8 @@
 class NfgCsvImporter::ImportsController < NfgCsvImporter::ApplicationController
-	include ActionView::Helpers::TextHelper
-	#expose(:import) do
-    #im = (params[:id].nil? ? Import.queued.new(import_params) : entity.imports.find_by_id(params[:id])).tap { |im| im.attributes = import_params unless request.get? }
-	#end
+  include ActionView::Helpers::TextHelper
+  #expose(:import) do
+  #im = (params[:id].nil? ? Import.queued.new(import_params) : entity.imports.find_by_id(params[:id])).tap { |im| im.attributes = import_params unless request.get? }
+  #end
 
   before_filter :load_entity
   before_filter :set_import_type
@@ -20,31 +20,33 @@ class NfgCsvImporter::ImportsController < NfgCsvImporter::ApplicationController
     end
   end
 
-	def index
+  def index
     @imports = @entity.imports.order_by_recent
-	end
+  end
 
-	def show
+  def show
     @import = @entity.imports.find(params[:id])
-	end
+  end
 
-	protected
-	# the standard event tracking (defined in application controller) attempts to include
-	# the imported file, which crashes the write to the db. So here we only track the type of import
-	def track_action
-	  ahoy.track "Processed #{controller_name}##{action_name}", { import_type: import_type }
-	end
+  protected
+  # the standard event tracking (defined in application controller) attempts to include
+  # the imported file, which crashes the write to the db. So here we only track the type of import
+  def track_action
+    ahoy.track "Processed #{controller_name}##{action_name}", { import_type: import_type }
+  end
 
-	def import_params
+  def import_params
     # params.require(:import).permit!
-		params.fetch(:import, {}).merge(import_type: @import_type).permit!
-	end
+    params.fetch(:import, {}).merge(import_type: @import_type).permit!
+  end
 
   def load_new_import
     @import ||= NfgCsvImporter::Import.queued.new(import_params)
   end
 
   def load_entity
+    raise Entity.all.inspect
+    raise NfgCsvImporter.configuration.entity_class.constantize.inspect
     @entity = NfgCsvImporter.configuration.entity_class.constantize.find_by(subdomain: request.subdomain)
   end
 
