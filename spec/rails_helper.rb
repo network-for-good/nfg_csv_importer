d8c9406c648ec6f8cc42b43b17b112f5de8469fb
Thomas Hoen <tom@givecorps.om>
2016-04-12 12:31:35 -0400

    mock with mocha

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,7 +12,7 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
-  config.mock_with :rspec
+  config.mock_with :mocha
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
   config.infer_spec_type_from_file_location!
