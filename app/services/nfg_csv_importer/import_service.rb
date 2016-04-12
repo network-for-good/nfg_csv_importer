991b37ca9defeb396ccb7c6e3fa2fd283b300c87
Thomas Hoen <tom@givecorps.om>
2016-04-12 12:33:55 -0400

    The import service sets the time zone for the import using the import objects time zone method

--- app/services/nfg_csv_importer/import_service.rb
+++ app/services/nfg_csv_importer/import_service.rb
@@ -66,6 +66,7 @@ class NfgCsvImporter::ImportService
   private
 
   def load_and_persist_imported_objects
+    Time.zone = import_record.time_zone
     self.errors_list = Array.new
     (2..spreadsheet.last_row).map do |i|
       row = Hash[[header, spreadsheet.row(i)].transpose]
@@ -189,5 +190,9 @@ class NfgCsvImporter::ImportService
         csv << error_hash.values
       end
     end
+  end
+
+  def import_record
+    NfgCsvImporter::Import.find(import_id)
   end
 end
