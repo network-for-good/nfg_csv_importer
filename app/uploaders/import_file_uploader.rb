942df3f33cd64c620ad474ce3e81fc40ddfe99cf
Thomas Hoen <tom@givecorps.om>
2016-05-20 16:34:22 -0400

    Allows the model name in the importer store_dir to be overridden

--- app/uploaders/import_file_uploader.rb
+++ app/uploaders/import_file_uploader.rb
@@ -1,15 +1,19 @@
 class ImportFileUploader < CarrierWave::Uploader::Base
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{class_name_for_store_dir}/#{mounted_as}/#{model.id}"
+  end
+
+  def class_name_for_store_dir
+    model.respond_to?(:class_name_for_store_dir) ? model.class_name_for_store_dir : model.class.to_s.underscore
   end
 
   def extension_white_list
     %w(csv xls xlsx)
   end
 
-  def filename 
-    if original_filename 
+  def filename
+    if original_filename
       @name ||= Digest::MD5.hexdigest(File.dirname(current_path))
       "#{@name}.#{file.extension}"
     end
