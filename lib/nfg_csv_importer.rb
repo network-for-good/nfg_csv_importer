56dde78f0a022a1fbffb8160b7473f7905f7efe1
Andrew Porterfield <ajporterfield@gmail.com>
2016-08-23 15:21:00 -0400

    DM-1569 Added optional max_run_time config that can be used to enqueue additional jobs if the we're getting close to timeout

--- lib/nfg_csv_importer.rb
+++ lib/nfg_csv_importer.rb
@@ -17,7 +17,7 @@ module NfgCsvImporter
 
   class Configuration
     attr_accessor :imported_for_class, :imported_by_class, :base_controller_class,
-                  :from_address, :reply_to_address, :imported_for_subdomain
+                  :from_address, :reply_to_address, :imported_for_subdomain, :max_run_time
 
     def imported_for_field
       (imported_for_class.downcase + "_id")
