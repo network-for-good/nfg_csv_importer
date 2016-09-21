f1eb0bb47a04fb4191113120e99cef924fd205da
Timothy King <tmk@lordzork.com>
2016-09-21 17:55:36 -0400

    Fixes typo and bumps version

--- app/jobs/nfg_csv_importer/destroy_import_job.rb
+++ app/jobs/nfg_csv_importer/destroy_import_job.rb
@@ -5,7 +5,7 @@ module NfgCsvImporter
     def perform(*args)
       batch = args.first
       import = NfgCsvImporter::Import.find(args.last)
-      import.update_attribute(:status, NfgCsvImporter::Import.status[:deleting])
+      import.update_attribute(:status, NfgCsvImporter::Import.statuses[:deleting])
       processing_final_batch = import.imported_records.size == batch.size
       batch.each do |imported_record_id|
         imported_record = NfgCsvImporter::ImportedRecord.find(imported_record_id) rescue next
@@ -15,7 +15,7 @@ module NfgCsvImporter
 
       if processing_final_batch
         NfgCsvImporter::ImportMailer.send_destroy_result(import).deliver
-        import.update_attribute(:status, NfgCsvImporter::Import.status[:deleted])
+        import.update_attribute(:status, NfgCsvImporter::Import.statuses[:deleted])
       end
     end
   end
