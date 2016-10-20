cd882c68f8259059a80beb2291d1bdb179d6822e
jonathanroehm <jonathan@givecorps.com>
2016-10-20 16:37:47 -0400

    adds helper spec outline

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe ApplicationHelper, type: :helper do
+
+  describe "#index_alphabetize" do
+    # subject { helper.index_alphabetize }
+    it "returns alphabetic characters" do
+      # send in [0,1,2,3]
+      # expect [A,B,C,D]
+    end
+  end
+end
