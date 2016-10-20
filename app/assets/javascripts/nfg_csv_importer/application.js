63dfad2e05e482fb92f798df18affe2b382e2c8d
jonathanroehm <jonathan@givecorps.com>
2016-10-20 12:33:56 -0400

    first pass at adding namespaced CSS to gem

--- app/assets/javascripts/nfg_csv_importer/application.js
+++ app/assets/javascripts/nfg_csv_importer/application.js
@@ -10,6 +10,33 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+
+//= require jquery
+// jquery_ujs
+// mousetrap
+// keybindings
+// turbolinks
+// handlebars-v4.0.5
+// jquery.ui.datepicker
+// jquery.ui.sortable
+// readmore.min
+// _tree ../../../vendor/assets/javascripts/redactor
+// Chart.min
+// twitter/typeahead
+// select2.min
+// blockUI.min
+// moment
+// bootstrap-datetimepicker
+//= require tether.min
+// accountingjs
+// jquery.sticky
+// _directory ../../../vendor/assets/javascripts/linkify
+
+// Bootstrap4
+//= require bootstrap4/bootstrap.min
+//= require_self
+//= require_directory ./
 //= require_tree .
 
 $(document).ready(function() {
