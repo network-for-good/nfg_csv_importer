62820780082f2040109e632735f638b633491593
jonathanroehm <jonathan@givecorps.com>
2016-10-20 17:18:57 -0400

    Adds spec outline for testing index_alphabetize

---
+++ spec/helpers/imports_helper_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe NfgCsvImporter::ImportsHelper do
+
+  describe "#index_alphabetize" do
+
+    it "returns alphabetic characters" do
+      # Goal: Pass in a few numbers from the index, specifically 0, 1, and 26
+      # get back... A,B, AA
+    end
+  end
+end
