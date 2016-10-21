9df2ace634594f12848e4a4a701edbd68eeac15c
jonathanroehm <jonathan@givecorps.com>
2016-10-21 09:20:36 -0400

    Updates helper spec to test the index_alphabetize method.

--- spec/helpers/imports_helper_spec.rb
+++ spec/helpers/imports_helper_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 describe NfgCsvImporter::ImportsHelper do
-
   describe "#index_alphabetize" do
-
     it "returns alphabetic characters" do
-      # Goal: Pass in a few numbers from the index, specifically 0, 1, and 26
-      # get back... A,B, AA
+      { 0 => 'A', 1 => 'B', 26 => 'AA' }.each do |index, value|
+        expect(helper.index_alphabetize[index]).to eq value
+      end
     end
   end
 end
