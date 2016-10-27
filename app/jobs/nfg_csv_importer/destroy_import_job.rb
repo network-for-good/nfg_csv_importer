638215155b637c56e5d2c7760c0d4c496622f076
Timothy King <tmk@lordzork.com>
2016-10-27 10:37:08 -0400

    Modifies ImportedRecord#destroy to handle importables with no imported_for association. Adds logging.

--- app/jobs/nfg_csv_importer/destroy_import_job.rb
+++ app/jobs/nfg_csv_importer/destroy_import_job.rb
@@ -6,15 +6,26 @@ module NfgCsvImporter
       batch = args.first
       import = NfgCsvImporter::Import.find(args.last)
       processing_final_batch = import.imported_records.size == batch.size
+      @stats = {}
       batch.each do |imported_record_id|
         imported_record = NfgCsvImporter::ImportedRecord.find(imported_record_id) rescue next
         next unless imported_record.created?
         imported_record.destroy
+        imported_record.destroy_stats.each do |key, value|
+          if @stats.has_key?(key)
+            @stats[key] << value
+          else
+            @stats[key] = [value]
+          end
+        end
       end
+
+      Rails.logger.info "Batch finished: #{@stats}"
 
       if processing_final_batch
         NfgCsvImporter::ImportMailer.send_destroy_result(import).deliver
         import.update_attribute(:status, NfgCsvImporter::Import.statuses[:deleted])
+        Rails.logger.info "Final batch for #{import.class} #{import.id} finished processing at #{DateTime.now}"
       end
     end
   end
--- app/models/nfg_csv_importer/imported_record.rb
+++ app/models/nfg_csv_importer/imported_record.rb
@@ -1,4 +1,6 @@
 class NfgCsvImporter::ImportedRecord < ActiveRecord::Base
+  attr_accessor :destroy_stats
+
   belongs_to :import
   belongs_to :imported_by, class_name: NfgCsvImporter.configuration.imported_by_class, foreign_key: :imported_by_id
   belongs_to :imported_for, class_name: NfgCsvImporter.configuration.imported_for_class, foreign_key: :imported_for_id
@@ -14,11 +16,13 @@ class NfgCsvImporter::ImportedRecord < ActiveRecord::Base
   end
 
   def destroy
+    self.destroy_stats = {}
     if self.importable.present?
       if importable.respond_to?(:can_be_destroyed?)
         if importable.can_be_destroyed?
           destroy_importable! && super
         else
+          log_importable_to(:undestroyable)
           return
         end
       else
@@ -36,7 +40,23 @@ class NfgCsvImporter::ImportedRecord < ActiveRecord::Base
   private
 
   def destroy_importable!
-    return false unless importable.send(NfgCsvImporter.configuration.imported_for_class.downcase).id == imported_for_id
+    imported_for_association = NfgCsvImporter.configuration.imported_for_class.downcase
+
+    unless importable.respond_to?(imported_for_association)
+      log_importable_to(:no_imported_for_assoc)
+      return false
+    end
+
+    unless importable.send(imported_for_association).id == imported_for_id
+      log_importable_to(:mismatched_owner_ids)
+      return false
+    end
+
     importable.destroy
+    log_importable_to(:destroyed)
+  end
+
+  def log_importable_to(stat_type)
+    destroy_stats[stat_type] = "#{importable_type}/#{importable_id}"
   end
 end
