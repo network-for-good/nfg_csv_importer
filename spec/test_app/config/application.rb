0f1be9330d75bc94cfb2a2d0519941e67279edc7
Timothy King <tmk@lordzork.com>
2016-10-28 14:25:03 -0400

    Fix to application.rb

--- spec/test_app/config/application.rb
+++ spec/test_app/config/application.rb
@@ -26,7 +26,7 @@ module TestApp
     # config.i18n.default_locale = :de
     config.default_from_address = 'noreply@networkforgood.com'
     config.action_mailer.default_url_options = { host: "test.com" }
-    config.routes.default_url_options = { host: 'test.com' }
+    routes.default_url_options = { host: 'test.com' }
   end
 end
 
