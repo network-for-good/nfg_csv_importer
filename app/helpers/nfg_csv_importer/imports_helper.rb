ccbbcb75eb6e495cdf2be15798f36bb15abc07aa
jonathanroehm <jonathan@givecorps.com>
2016-11-11 13:30:05 -0500

    Updates helper methods to better account for disabling links, adds a link for editing the import so that it is only available (and not disabled) when the import has the uploaded status.

--- app/helpers/nfg_csv_importer/imports_helper.rb
+++ app/helpers/nfg_csv_importer/imports_helper.rb
@@ -6,52 +6,89 @@ module NfgCsvImporter
     end
 
     def import_status_icon_and_text(import)
+      import_status_class = "m-r-quarter"
       case import.status.to_sym
       when :uploaded
         import_status_icon = "cloud-upload"
-        import_status_class = "text-primary"
+        import_status_class += " text-primary"
       when :defined
         import_status_icon = "table"
-        import_status_class = "text-primary"
+        import_status_class += " text-primary"
       when :queued
         import_status_icon = "hourglass-2"
-        import_status_class = "text-warning"
+        import_status_class += " text-warning"
       when :processing
         import_status_icon = "refresh"
-        import_status_class = "text-warning"
+        import_status_class += " text-warning"
       when :complete
         import_status_icon = "check"
-        import_status_class = "text-success"
+        import_status_class += " text-success"
       when :deleting
         import_status_icon = "trash-o"
-        import_status_class = "text-danger"
+        import_status_class += " text-danger"
       when :deleted
         import_status_icon = "trash-o"
-        import_status_class = "text-danger"
+        import_status_class += " text-danger"
       end
 
-      link_to(import_path(import), class: import_status_class) do
-        "#{fa_icon(import_status_icon, class: ['m-r-quarter', import_status_class].join(' '))} #{import.status.titleize}".html_safe
+      link_to import_path(import), class: import_status_class do
+        "#{ fa_icon import_status_icon, class: import_status_class } #{ import.status.titleize }".html_safe
       end
     end
 
     def number_of_records_with_errors_based_on_import_status(import)
-      if import.status.to_sym == :uploaded
-        fa_icon("minus", class: "text-muted").html_safe
-      elsif import.error_file.file.present?
-        html = "<span style='text-danger'>#{import.number_of_records_with_errors.to_i}</span>".html_safe
-        html += "<small>#{fa_icon 'paperclip', class: 'text-danger'} #{link_to "Error File", import.error_file.url, class: 'text-danger'}</small>".html_safe
+      error_records_count = import.number_of_records_with_errors.to_i
+      import_with_errors_color_class = "m-r-quarter"
+
+      if error_records_count > 0
+        if import.status.to_sym == :processing
+          error_file_link_url = "javascript:;"
+          import_with_errors_color_class += " text-muted"
+          error_file_link_style = "pointer-events: none;"
+          error_file_link_tabindex = "-1"
+        else
+          error_file_link_url = import.error_file.url
+          import_with_errors_color_class += " text-danger"
+          error_file_link_style = ""
+          error_file_link_tabindex = nil
+        end
+
+        html = "<h4 class='text-danger'>#{ error_records_count }</h4>"
+        html += "<small>"
+        html += "#{ fa_icon 'paperclip', class: import_with_errors_color_class }"
+        html += "#{ link_to "Error File", error_file_link_url, class: import_with_errors_color_class, style: error_file_link_style, tabindex: error_file_link_tabindex }"
+        html += "</small>"
       else
-        html = import.number_of_records_with_errors.to_i
+        html = fa_icon "minus", class: "text-muted"
       end
-      html
+      html.html_safe
     end
 
     def number_of_records_without_errors_based_on_import_status(import)
       if import.status.to_sym == :uploaded
-        fa_icon("minus", class: "text-muted").html_safe
+        html = fa_icon "minus", class: "text-muted"
       else
-        import.number_of_records
+        html = "<h4>#{ import.number_of_records }</h4>"
+      end
+      html.html_safe
+    end
+
+    def edit_import_link(import)
+      edit_import_link_class = "btn btn-link"
+
+      if import.status.to_sym == :uploaded
+        edit_import_link_url = edit_import_path(import)
+        edit_import_link_icon_class = ""
+        edit_import_link_tab_index = nil
+      else
+        edit_import_link_url = "javascript:;"
+        edit_import_link_class += " disabled"
+        edit_import_link_icon_class = "text-muted"
+        edit_import_link_tab_index = "-1"
+      end
+
+      link_to edit_import_link_url, class: edit_import_link_class, tabindex: edit_import_link_tab_index do
+        "#{ fa_icon 'pencil', class: edit_import_link_icon_class } Edit Import".html_safe
       end
     end
   end
