ef1f909c67341f2c2ebcda712e46802aa0a57efb
jonathanroehm <jonathan@givecorps.com>
2016-11-14 14:03:27 -0500

    Adjustments to records output helpers for the index page. Unifies elements and adds necessary styles.

--- app/helpers/nfg_csv_importer/imports_helper.rb
+++ app/helpers/nfg_csv_importer/imports_helper.rb
@@ -38,31 +38,30 @@ module NfgCsvImporter
 
     def number_of_records_with_errors_based_on_import_status(import)
       error_records_count = import.number_of_records_with_errors.to_i
-      import_with_errors_color_class = "m-r-quarter"
 
       if error_records_count > 0
         if import.status.try(:to_sym) == :processing
           error_file_link_url = "javascript:;"
-          import_with_errors_color_class += " text-muted"
+          import_with_errors_color_class = "text-body"
           error_file_link_style = "pointer-events: none;"
           error_file_link_tabindex = "-1"
         else
           error_file_link_url = import.error_file.url
-          import_with_errors_color_class += " text-danger"
+          import_with_errors_color_class = "text-danger"
           error_file_link_style = ""
           error_file_link_tabindex = nil
         end
 
         html =
-          "<h4 class='text-danger'>#{ error_records_count }</h4>
-           <small>
+          "<h5 class='text-danger line-height-1 m-b-quarter'>#{ error_records_count }</h4>
+           <p>
              #{ fa_icon 'paperclip', class: import_with_errors_color_class }
              #{ link_to "Error File", error_file_link_url, class: import_with_errors_color_class, style: error_file_link_style, tabindex: error_file_link_tabindex }
-           </small>"
-
+           </p>"
       else
         html = fa_icon "minus", class: "text-muted"
       end
+
       html.html_safe
     end
 
