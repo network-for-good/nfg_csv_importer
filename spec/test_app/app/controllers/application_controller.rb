f7e062f4e8645e574a1d4865706e5426352a1afb
Thomas Hoen <tom@givecorps.om>
2017-01-27 09:50:50 -0500

    Fixes font-awesome icons on column mappings

--- spec/test_app/app/controllers/application_controller.rb
+++ spec/test_app/app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def entity
-    @entity ||= (Entity.find_by(subdomain: request.subdomain) || Entity.first)
+    @entity ||= (Entity.find_by(subdomain: request.subdomain) || Entity.first || Entity.create(subdomain: "test"))
   end
 
   def current_user
-    @current_user ||= entity.users.first
+    @current_user ||= entity.users.first || User.create(first_name: "Jim", last_name: "Smith", email: "jim@smith.com")
   end
   helper_method :current_user
 end
