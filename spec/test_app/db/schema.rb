aad0a128e9a45ebdf3f37ae7938b14b381774a4d
Pavan <kcpavan@me.com>
2019-05-21 08:49:40 +0530

    Add active storage migrations

--- spec/test_app/db/schema.rb
+++ spec/test_app/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,62 +10,82 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170130141053) do
+ActiveRecord::Schema.define(version: 2019_05_20_173436) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "entities", force: :cascade do |t|
-    t.string   "subdomain"
+    t.string "subdomain"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "nfg_csv_importer_imported_records", force: :cascade do |t|
-    t.integer  "imported_for_id"
-    t.integer  "imported_by_id"
-    t.string   "action"
-    t.integer  "importable_id"
-    t.string   "importable_type"
-    t.string   "transaction_id"
+    t.integer "imported_for_id"
+    t.integer "imported_by_id"
+    t.string "action"
+    t.integer "importable_id"
+    t.string "importable_type"
+    t.string "transaction_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "import_id"
-    t.boolean  "deleted",         default: false
-    t.text     "row_data"
+    t.integer "import_id"
+    t.boolean "deleted", default: false
+    t.text "row_data"
+    t.index ["import_id"], name: "index_nfg_csv_importer_imported_records_on_import_id"
   end
 
-  add_index "nfg_csv_importer_imported_records", ["import_id"], name: "index_nfg_csv_importer_imported_records_on_import_id"
-
   create_table "nfg_csv_importer_imports", force: :cascade do |t|
-    t.string   "import_type"
-    t.string   "import_file"
-    t.string   "error_file"
-    t.integer  "number_of_records"
-    t.integer  "number_of_records_with_errors"
-    t.integer  "imported_for_id"
-    t.integer  "imported_by_id"
-    t.integer  "status"
-    t.integer  "records_processed"
+    t.string "import_type"
+    t.string "import_file"
+    t.string "error_file"
+    t.integer "number_of_records"
+    t.integer "number_of_records_with_errors"
+    t.integer "imported_for_id"
+    t.integer "imported_by_id"
+    t.integer "status"
+    t.integer "records_processed"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text     "fields_mapping"
-    t.string   "import_file_name"
+    t.text "fields_mapping"
+    t.string "import_file_name"
     t.datetime "processing_started_at"
     t.datetime "processing_finished_at"
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string  "name"
-    t.string  "description"
+    t.string "name"
+    t.string "description"
     t.integer "cause_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.integer  "entity_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.integer "entity_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "note"
+    t.string "note"
   end
 
 end
