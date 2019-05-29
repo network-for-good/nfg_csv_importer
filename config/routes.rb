1ebbca3fcf19ad619e1dc0659a3bb3ae0b714bad
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-05-29 15:41:13 -0400

    Commits the pre processes controller and get started action in its initial setup while other workings and goings on are organized for commit.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ NfgCsvImporter::Engine.routes.draw do
     resource :process, only: :create
     get :template, on: :collection
   end
+
+
+  get 'pre_processes/get_started', to: 'pre_processes#get_started', as: 'pre_processes_get_started'
+
 end
