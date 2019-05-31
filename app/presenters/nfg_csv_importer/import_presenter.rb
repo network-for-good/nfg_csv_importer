c80550588dd11bf09d687d6e5fece5b15d83c729
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-05-31 10:15:04 -0400

    Fixes broken modal text

--- app/presenters/nfg_csv_importer/import_presenter.rb
+++ app/presenters/nfg_csv_importer/import_presenter.rb
@@ -5,8 +5,8 @@ module NfgCsvImporter
     end
 
     def get_started_modal_steps(step:, heading_options: {}, body_options: {})
-      h.ui.nfg(:typeface, heading: I18n.t("nfg_csv_importer.pre_processes.get_started_modal.step#{step}.heading", **heading_options)) +
-      h.ui.nfg(:typeface, body: I18n.t("nfg_csv_importer.pre_processes.get_started_modal.step#{step}.body", **body_options))
+      h.ui.nfg(:typeface, heading: I18n.t("nfg_csv_importer.pre_processes.get_started_modal.pre_processing_type.step#{step}.heading", **heading_options)) +
+      h.ui.nfg(:typeface, body: I18n.t("nfg_csv_importer.pre_processes.get_started_modal.pre_processing_type.step#{step}.body", **body_options))
     end
   end
 end
