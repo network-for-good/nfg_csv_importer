b14e18a6e9436655c85381af9387b300db64fb51
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-05-31 12:22:40 -0400

    Adds note about precompile.

--- lib/nfg_csv_importer/engine.rb
+++ lib/nfg_csv_importer/engine.rb
@@ -2,6 +2,12 @@ module NfgCsvImporter
   class Engine < ::Rails::Engine
     isolate_namespace NfgCsvImporter
 
+    # This allows us to declare folders and assets for precompilation
+    # to be consumed by host apps. To update precompilation assets
+    # edit: app/assets/config/nfg_csv_importer_manifest.js
+    #
+    # Utilize sprockets syntax, ex:
+    # //= link_tree ../images/nfg_csv_importer
     initializer 'nfg_csv_importer.assets.precompile' do |app|
       app.config.assets.precompile << "#{Engine.root.join('app', 'assets', 'config')}/nfg_csv_importer_manifest.js"
     end
