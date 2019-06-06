abcd541933247132341d9740f67be727be9a343a
Thomas Hoen <tom@givecorps.com>
2019-06-06 12:21:16 -0400

    adds file_origination_types to the configuration object

--- lib/nfg_csv_importer.rb
+++ lib/nfg_csv_importer.rb
@@ -22,7 +22,7 @@ module NfgCsvImporter
 
   class Configuration
     attr_accessor :imported_for_class, :imported_by_class, :base_controller_class,
-                  :from_address, :reply_to_address, :imported_for_subdomain, :max_run_time
+                  :from_address, :reply_to_address, :imported_for_subdomain, :max_run_time, :file_origination_types
 
     def imported_for_field
       (imported_for_class.downcase + "_id")
