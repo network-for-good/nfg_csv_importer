a7f82029460a48893e1eea46c7e7f95613bb8e25
Thomas Hoen <tom@givecorps.com>
2019-06-10 10:32:27 -0400

    renames the pre_processing import column to file_origination

--- db/migrate/20190529133332_add_file_origination_type_to_imports.rb
+++ db/migrate/20190529133332_add_file_origination_type_to_imports.rb
@@ -1,5 +1,5 @@
 class AddPreProcessingTypeToImports < ActiveRecord::Migration[5.2]
   def change
-    add_column :nfg_csv_importer_imports, :pre_processing_type, :string
+    add_column :nfg_csv_importer_imports, :file_origination_type, :string
   end
 end
