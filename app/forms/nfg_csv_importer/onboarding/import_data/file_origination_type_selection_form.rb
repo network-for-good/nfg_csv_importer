8456b1914d0db1ff974ce74395bf6643a2a4393a
Thomas Hoen <tom@givecorps.com>
2019-06-10 10:43:36 -0400

    Ensure file_origination_type is being used where pre_processing_type was before

--- app/forms/nfg_csv_importer/onboarding/import_data/file_origination_type_selection_form.rb
+++ app/forms/nfg_csv_importer/onboarding/import_data/file_origination_type_selection_form.rb
@@ -1,4 +1,4 @@
 class  NfgCsvImporter::Onboarding::ImportData::FileOriginationTypeSelectionForm < NfgCsvImporter::Onboarding::ImportData::BaseForm
   ## Add properties for your form below:
-  property :pre_processing_type
+  property :file_origination_type
 end
