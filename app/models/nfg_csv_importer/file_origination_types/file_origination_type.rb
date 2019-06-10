6dacf19e69571329ed934a9ab6c8fc19f5f9ef5d
Thomas Hoen <tom@givecorps.com>
2019-06-09 21:06:00 -0400

    Adds the file_origination_type manager, file origination type, and updates the base file_origination _type

---
+++ app/models/nfg_csv_importer/file_origination_types/file_origination_type.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module NfgCsvImporter
+  module FileOriginationTypes
+    class FileOriginationType
+      attr_accessor :name, :requires_preprocessing_files, :allowed_import_types, :post_preprocessing_upload_hook, :field_mapping,
+                    :expects_preprocessing_to_attach_post_processing_file, :logo_path, :type_sym, :description
+
+      def initialize(file_type_sym, type_klass)
+        @type_sym = file_type_sym
+        @type_klass = type_klass
+        self.name = type_klass.name
+        self.logo_path = type_klass.logo_path
+        self.description = type_klass.description
+      end
+    end
+  end
+end
