a2af202e5475a4ef99b403677df2de1dec01bf24
Thomas Hoen <tom@givecorps.com>
2019-06-12 17:26:49 -0400

    Couple little fixes

--- app/models/nfg_csv_importer/file_origination_types/self_import_csv_xls.rb
+++ app/models/nfg_csv_importer/file_origination_types/self_import_csv_xls.rb
@@ -9,6 +9,23 @@ module FileOriginationTypes
       def description
         'Every single contact can be brought over to donor management.'
       end
+
+      def requires_preprocessing_files
+        # If this file origination type expects the user to supply
+        # pre-processing files to be worked on by the system or handed
+        # off for manual manipulation, return true. Returning false will
+        # skip the preprocessing upload
+        false
+      end
+
+      def expects_preprocessing_to_attach_post_processing_file
+        # For files that will be preprocessed, there is an expectation that
+        # the output will be a postprocessed file that will be attached
+        # to the Import record (import.file)
+        # If this value is false, the user will be required to supply the
+        # post processed file.
+        false
+      end
     end
   end
 end
