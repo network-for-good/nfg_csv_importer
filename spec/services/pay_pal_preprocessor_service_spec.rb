fde1697f69557d61919e13a10b0daaf0a3cf7e54
Pavan <kcpavan@me.com>
2019-06-13 15:33:14 +0530

    [#DM-5291] Added basic structure for paypal donations preprocessing

---
+++ spec/services/pay_pal_preprocessor_service_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe PayPalPreprocessorService do
+  include ActionDispatch::TestProcess
+
+  let(:import) { FactoryGirl.create(:import,
+                                  pre_processing_files: fixture_file_upload(File.open("spec/fixtures/PayPal_donations.xlsx")))}
+  let(:service) { PayPalPreprocessorService.new(import) }
+
+  describe '#process' do
+    subject { service.process }
+
+    it "should pre process and attach import_file" do
+      subject
+      import.reload
+      expect(CSV.open(import.import_file.path).readlines).to eq  CSV.open('spec/fixtures/paypal_processed_file.csv').readlines
+      expect(import.status).to eq 'uploaded'
+    end
+  end
+end
