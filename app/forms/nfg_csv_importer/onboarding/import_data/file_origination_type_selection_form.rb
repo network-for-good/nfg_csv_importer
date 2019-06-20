a16bac9eb0dea0f0b5a7fc7964aea313811495ff
Thomas Hoen <tom@givecorps.com>
2019-06-20 14:04:52 -0400

    Ensure that the file origination type gets selected

--- app/forms/nfg_csv_importer/onboarding/import_data/file_origination_type_selection_form.rb
+++ app/forms/nfg_csv_importer/onboarding/import_data/file_origination_type_selection_form.rb
@@ -1,4 +1,6 @@
 class  NfgCsvImporter::Onboarding::ImportData::FileOriginationTypeSelectionForm < NfgCsvImporter::Onboarding::ImportData::BaseForm
   ## Add properties for your form below:
   property :file_origination_type
+
+  validates_presence_of :file_origination_type
 end
