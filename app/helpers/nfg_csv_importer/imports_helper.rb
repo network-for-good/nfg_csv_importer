6c4dc6c4a54095642c5dbce247bc4a7fce50782a
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-06-20 17:05:13 -0400

    lays in the overview step for UI and adds a fake import definition to test app for use on the overview page for testing.

--- app/helpers/nfg_csv_importer/imports_helper.rb
+++ app/helpers/nfg_csv_importer/imports_helper.rb
@@ -109,7 +109,7 @@ module NfgCsvImporter
 
     def import_column_display_name(import_type, column, titleize = false)
       display_name = ""
-      display_name = t("imports.column_display_names.#{import_type}.#{column}", default: column) if column.present?
+      display_name = I18n.t("imports.column_display_names.#{import_type}.#{column}", default: column) if column.present?
       display_name = display_name.downcase.tr("_", " ").titleize if titleize
       display_name
     end
--- spec/test_app/app/imports/import_definition.rb
+++ spec/test_app/app/imports/import_definition.rb
@@ -3,13 +3,13 @@ class ImportDefinition < NfgCsvImporter::ImportDefinition
   include NfgCsvImporter::WorkingCode::ImportDefinitionUpdateExamples
 
   def self.import_types
-    %w{users donation}
+    %w{users donation users_for_testing_overview}
   end
 
   attr_accessor :imported_for
   def users
     {
-      required_columns: %w{ email },
+      required_columns: %w{email},
       optional_columns: %w{first_name last_name full_name tribue_attribute_and_notification_preference_with_salutation surname wealth_score college_degree last_donation_date note },
       default_values: { "first_name" => lambda { |row| row["email"].try(:split, "@").try(:first) } },
       field_aliases: { "first_name" => ["first", "donor first name"],
@@ -33,6 +33,44 @@ class ImportDefinition < NfgCsvImporter::ImportDefinition
           'surname' => 'Must be either m or f (m = male, f = female)',
           'wealth_score' => "Use this value to ensure that multiple donation records get attributed to a single donor. All records with the same external_user_id will be attached to one donor record using the information in the first row containing that external_user_id",
           'college_degree' => "Notes are displayed on the contact's profile"
+        },
+      description: %Q{Allows you to import subscribers.},
+      can_be_deleted_by: -> (user) { user.last_name != "Jones" },
+
+      **NfgCsvImporter::WorkingCode::ImportDefinitionUpdateExamples.user_humanized_data_set,
+
+      **NfgCsvImporter::WorkingCode::ImportDefinitionUpdateExamples.user_summary_data_set
+    }
+  end
+
+  def users_for_testing_overview
+    {
+      required_columns: %w{email wealth_score college_degree last_donation_date note},
+      optional_columns: %w{first_name last_name full_name tribue_attribute_and_notification_preference_with_salutation surname wealth_score college_degree last_donation_date note},
+      default_values: { "first_name" => lambda { |row| row["email"].try(:split, "@").try(:first) } },
+      field_aliases: { "first_name" => ["first", "donor first name"],
+                      "last_name" => ["last", "donor last name"],
+                      "email" => ["email"]
+                      },
+      column_validation_rules: [
+                                  { type: "any",
+                                    fields: ["first_name", "full_name"],
+                                    message: "You must map a column to either <strong>First Name</strong> or <strong>Full Name</strong>"
+                                  }
+                                ],
+      fields_that_allow_multiple_mappings: ["note"],
+      class_name: "User",
+      alias_attributes: [],
+      column_descriptions: {
+          'email' => 'Their email address',
+          'last_donation_date' => 'When the last donation occurred',
+          'first_name' => "The numeric internal ID of the individual record. Only empty fields will be updated. New data will not overwrite existing data. To update donation records, please use the update_donation importer.",
+          'last_name' => 'Must be a valid email address',
+          'full_name' => "If other address fields are included, and country is left blank, it will be set to 'US'",
+          'tribue_attribute_and_notification_preference_with_salutation' => "Mr, Mrs, Ms, Dr, etc",
+          'surname' => 'Must be either m or f (m = male, f = female)',
+          'wealth_score' => "Use this value to ensure that multiple donation records get attributed to a single donor. All records with the same external_user_id will be attached to one donor record using the information in the first row containing that external_user_id",
+          'college_degree' => "Where they went to college"
         },
       description: %Q{Allows you to import subscribers.},
       can_be_deleted_by: -> (user) { user.last_name != "Jones" },
