2db21f166f740ec88bb742a42eee5cff448a1e0a
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-06-24 15:49:23 -0400

    simplifies code setup and removes WorkingCode for preview confirmation step.

--- lib/nfg_csv_importer/working_code.rb
+++
@@ -1,7 +0,0 @@
-module NfgCsvImporter
-  module WorkingCode
-    require_relative 'working_code/import_definition_update_examples'
-    require_relative 'working_code/import_definition_details_updates'
-    require_relative 'working_code/fake_paypal_import'
-  end
-end
