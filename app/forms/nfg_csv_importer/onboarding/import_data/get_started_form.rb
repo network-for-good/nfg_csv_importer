e9c08a9d2305ddfacf445646f6ab78373bbaca4a
Thomas Hoen <tom@givecorps.com>
2019-06-26 10:37:02 -0400

    Remove references to the get_started step and move the import type selection before the overview page

--- app/forms/nfg_csv_importer/onboarding/import_data/get_started_form.rb
+++
@@ -1,4 +0,0 @@
-class  NfgCsvImporter::Onboarding::ImportData::GetStartedForm < NfgCsvImporter::Onboarding::ImportData::BaseForm
-  ## Add properties for your form below:
-  # property :my_property
-end
