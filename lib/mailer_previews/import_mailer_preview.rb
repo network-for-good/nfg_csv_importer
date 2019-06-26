febf8ee9b3599471f1efbecbd6c1ebf05c09de6e
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-06-26 15:33:57 -0400

    mailer preview works.

--- lib/mailer_previews/import_mailer_preview.rb
+++ lib/mailer_previews/import_mailer_preview.rb
@@ -1,25 +1,25 @@
 class NfgCsvImporter::ImportMailerPreview < ActionMailer::Preview
   require 'nfg_csv_importer/mailer_utilities/preview_mailer_controller_transactions_patch'
 
-  def send_import_result
-    @status = NfgCsvImporter::ImportMailer::COMPLETED_STATUS
-
-    NfgCsvImporter::ImportMailer.send_import_result(import, @status)
-  end
-
-  def send_import_begins_result
+  def send_import_result_begins
     @status = NfgCsvImporter::ImportMailer::BEGIN_STATUS
 
     NfgCsvImporter::ImportMailer.send_import_result(import, @status)
   end
 
-  def send_import_queued_result
+  def send_import_result_queued
     @status = NfgCsvImporter::ImportMailer::QUEUED_STATUS
 
     NfgCsvImporter::ImportMailer.send_import_result(import, @status)
   end
 
-  def send_import_completed_result_with_errors
+  def send_import_result_completed
+    @status = NfgCsvImporter::ImportMailer::COMPLETED_STATUS
+
+    NfgCsvImporter::ImportMailer.send_import_result(import(error_file: true), @status)
+  end
+
+  def send_import_result_completed_with_errors
     @status = NfgCsvImporter::ImportMailer::COMPLETED_STATUS
 
     NfgCsvImporter::ImportMailer.send_import_result(import(error_file: true), @status)
@@ -28,21 +28,24 @@ class NfgCsvImporter::ImportMailerPreview < ActionMailer::Preview
   private
 
   def recipient
-    @recipient = OpenStruct.new(email: 'test@example.com', id: 1)
+    @recipient ||= FactoryGirl.create(:user, entity: entity)
+  end
+
+  def entity
+    @entity ||= FactoryGirl.create(:entity)
   end
 
   def import(error_file: false)
     @import ||= NfgCsvImporter::Import.create!(
-      id: 1,
       import_type: 'donation',
       import_file: File.open("#{NfgCsvImporter::Engine.root}/spec/fixtures/paypal_processed_file.csv"),
       error_file: (error_file ? File.open("#{NfgCsvImporter::Engine.root}/spec/fixtures/errors.xls") : nil),
       number_of_records: 39,
       number_of_records_with_errors: (error_file ? 1 : nil),
-      imported_for_id: 1,
-      imported_by: @recipient,
+      imported_for: entity,
+      imported_by: recipient,
       status: @status,
-      record_processed: (@status == NfgCsvImporter::ImportMailer::COMPLETED_STATUS ? 39 : nil),
+      records_processed: (@status == NfgCsvImporter::ImportMailer::COMPLETED_STATUS ? 39 : nil),
       created_at: Time.now - 10.minutes,
       import_file_name: 'paypal_processed_file.csv',
       processing_started_at: (@status == NfgCsvImporter::ImportMailer::QUEUED_STATUS ? nil : Time.now),
