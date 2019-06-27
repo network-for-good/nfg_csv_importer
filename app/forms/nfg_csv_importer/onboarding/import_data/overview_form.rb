624dd3b0bf676a20ea2af38705a1d81e2ca8f47e
Thomas Hoen <tom@givecorps.com>
2019-06-27 10:23:20 -0400

    Reformat the import onboarder forms to match rubocops preferred syntax

--- app/forms/nfg_csv_importer/onboarding/import_data/overview_form.rb
+++ app/forms/nfg_csv_importer/onboarding/import_data/overview_form.rb
@@ -1,4 +1,12 @@
-class  NfgCsvImporter::Onboarding::ImportData::OverviewForm < NfgCsvImporter::Onboarding::ImportData::BaseForm
-  ## Add properties for your form below:
-  # property :my_property
+# frozen_string_literal: true
+
+module NfgCsvImporter
+  module Onboarding
+    module ImportData
+      class  OverviewForm < NfgCsvImporter::Onboarding::ImportData::BaseForm
+        ## Add properties for your form below:
+        # property :my_property
+      end
+    end
+  end
 end
