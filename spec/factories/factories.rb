d916a3c1d52c44e55a9185490b4c669fd2a23e5d
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-06-27 10:52:42 -0400

    Sets up initial spec for the import mailer presenter. Adds import factories with traits to facilitate testing.

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -11,7 +11,38 @@ FactoryGirl.define do
     association :imported_for, factory: :entity
     association :imported_by, factory: :user
     import_file { File.open("spec/fixtures/subscribers.csv") }
-    import_type 'users'
-    status nil
+    import_type { 'users' }
+    status { nil }
+
+    trait :is_paypal do
+      file_origination_type { 'paypal' }
+      import_file { File.open("spec/fixtures/paypal_processed_file.csv") }
+      import_type { 'donation' }
+    end
+
+    trait :is_complete_with_errors do
+      is_complete
+      error_file { File.open("spec/fixtures/errors.xls") }
+      number_of_records_with_errors { CSV.foreach(error_file, headers: true).count }
+    end
+
+    trait :is_processing do
+      status { :processing }
+      processing_started_at { 10.minutes.ago }
+    end
+
+    trait :is_complete do
+      status { :complete }
+      processing_started_at { 10.minutes.ago }
+      processing_finished_at { 1.minute.ago }
+      records_processed { CSV.foreach(import_file, headers: true).count }
+    end
+
+    trait :is_queued do
+      status { :queued }
+      processing_started_at { 10.minutes.ago }
+    end
+
+
   end
 end
