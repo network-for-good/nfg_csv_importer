be5b3843e70dc25543687bf422a2de3abfb6a752
Thomas Hoen <tom@givecorps.com>
2019-06-28 10:33:36 -0400

    See if circle likes this file format better

--- spec/support/spec_helper_methods.rb
+++ spec/support/spec_helper_methods.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
+
 def create_test_upload_file(file_name, columns, rows)
   file_path = get_test_upload_file_path(file_name)
   CSV.open(file_path, "wb") do |test_file|
     test_file << columns
-    for row in rows
+    rows.each do |row|
       test_file << row
     end
   end
   file_path
 end
 
-
-  def get_test_upload_file_path(file_name = "pt_org_members_valid.txt")
-    f = File.expand_path(File.join(Rails.root, '/tmp/', file_name))
-  end
+def get_test_upload_file_path(file_name = "pt_org_members_valid.txt")
+  f = File.expand_path(Rails.root.join('/tmp/', file_name))
+end
