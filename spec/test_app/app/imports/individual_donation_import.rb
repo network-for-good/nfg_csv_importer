da47d0a937a824e9aa5d290d9e4d18d540c7c961
Thomas Hoen <tom@givecorps.com>
2019-07-02 18:00:24 -0400

    Gets the import to work for paypal file origination types

--- spec/test_app/app/imports/individual_donation_import.rb
+++ spec/test_app/app/imports/individual_donation_import.rb
@@ -1,7 +1,29 @@
 class IndividualDonationImport
   include ActiveModel::Model
 
-  attr_accessor :date,:time,:zone,:name,:amount,:email,:transaction_id,:address,:address_2,:city,:state,:zip_code,:country,:home_phone,:description
+  def self.attr_list
+    ["date","time","zone","name","amount","email","transaction_id","address","address_2","city","state","zip_code","country","home_phone","description"]
+  end
+
+  attr_accessor *attr_list
+
+  def has_attribute?(attribute)
+    respond_to? attribute
+  end
+
+  def attributes
+    Hash[self.class.attr_list.map { |x| [x, nil] }]
+  end
+
+  def attributes=(args)
+    args.each do |arg, value|
+      self.send("#{arg}=", value)
+    end
+  end
+
+  def self.table_name
+    User.table_name
+  end
 
   def save
     first_name, last_name = name.split
