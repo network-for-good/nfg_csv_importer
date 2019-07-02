81ead227b435448ee49a5577a390991fc726de87
Thomas Hoen <tom@givecorps.com>
2019-07-02 18:00:57 -0400

    Full paypal file origination spec now working

--- spec/features/importing_paypal_data_with_nfg_onboarder_spec.rb
+++ spec/features/importing_paypal_data_with_nfg_onboarder_spec.rb
@@ -35,10 +35,44 @@ describe "Using the nfg_onboarder engine to import paypal transactions", js: tru
       drop_in_dropzone(File.expand_path("spec/fixtures/paypal_sample_file.xlsx"))
       sleep 2
       expect { click_button 'Next' }.to change(NfgCsvImporter::Import, :count).by(1)
+      @import = NfgCsvImporter::Import.last
     end
 
     and_it 'takes the user to the preview_confirmation page' do
       expect(page).to have_css "body.nfg_csv_importer-onboarding-import_data.preview_confirmation"
+      # Since we are making perform_later happen inline here, we go all the way through to complete
+      # in production, it would only be queued
+    end
+
+    and_by 'confirming the preview confirmation page it should kick off the import' do
+      expect { click_button 'Next' }.to change { @import.reload.status }.from("uploaded")
+    end
+
+    and_by 'moving to the finish page' do
+      expect(page).to have_css "body.nfg_csv_importer-onboarding-import_data.finish.#{file_origination_type}"
+
+      # this is a bit hacky, but i wanted to make sure
+      # the import was finished before testing
+      # for some proof that it was successful
+      # on the page.
+      # In reality, we may want to assume that the
+      # job has been queued and not provide any
+      # indication that this page could display
+      # some results from the import, but instead
+      # note that the job has been enqueued and perhaps
+      # let the user know how many other jobs are in
+      # from of it
+      until @import.reload.complete? do
+        # wait until the import has completed
+        sleep 0.5
+      end
+      # reload the page once the import has completed
+      page.evaluate_script 'window.location.reload()'
+
+      # since the import has already completed (which will unlikely be the case in production)
+      # we show how many records were added
+      # In production, we will likely have different messages depending on the status of the import
+      expect(page).to have_content "You have successfully imported 4 records"
     end
   end
 end
