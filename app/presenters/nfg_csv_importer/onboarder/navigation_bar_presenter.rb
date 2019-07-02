bb558446604387b6f707038279106e8c875a33ce
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-07-02 12:16:06 -0400

    Removes links when on last step.

--- app/presenters/nfg_csv_importer/onboarder/navigation_bar_presenter.rb
+++ app/presenters/nfg_csv_importer/onboarder/navigation_bar_presenter.rb
@@ -3,28 +3,31 @@ module NfgCsvImporter
     # Ex: NfgCsvImporter::Onboarder::NavigationBarPresenter.new(onboarding_session)
     class NavigationBarPresenter < NfgCsvImporter::Onboarder::OnboarderPresenter
 
-      # Returns an nfg_ui friendly status for the :step component's status trait
-      #
-      # Example usage:
-      # = ui.nfg :step, onboarder_presenter.step_status(the_step), step: 1, href: wizard_path(the_step)
-      def step_status(step)
-        return :active if step.to_sym == active_step.to_sym
-        return :visited if try(:completed_steps, h.controller_name).try(:include?, step)
-        # in case steps change, if the step or active step can't be found in what
-        # is currently the list of steps (based on the file origination type) don't barf
-        return :disabled if (all_steps.index(step.to_sym) || 0) > (all_steps.index(active_step.to_sym) || 0)
+      # On the last step, provide nil to the requesting :href
+      # to ensure the steps aren't rendered as <a> tags.
+      # and thus, don't link anywhere semantically.
+      def step_href(step, path)
+        step == last_step ? nil : path
       end
 
       def step_icon(step)
         step == last_step ? 'check' : nil
       end
 
-      def show_step?(step)
-        true
-      end
-
       def show_nav?
         active_step.to_sym != all_steps.first
+      end
+
+      # Returns an nfg_ui friendly status for the :step component's status trait
+      #
+      # Example usage:
+      # = ui.nfg :step, onboarder_presenter.step_status(the_step), step: 1, ...
+      def step_status(step)
+        return :active if step.to_sym == active_step.to_sym
+        return :visited if try(:completed_steps, h.controller_name).try(:include?, step)
+        # in case steps change, if the step or active step can't be found in what
+        # is currently the list of steps (based on the file origination type) don't barf
+        return :disabled if (all_steps.index(step.to_sym) || 0) > (all_steps.index(active_step.to_sym) || 0)
       end
     end
   end
--- spec/presenters/nfg_csv_importer/onboarder/navigation_bar_presenter_spec.rb
+++ spec/presenters/nfg_csv_importer/onboarder/navigation_bar_presenter_spec.rb
@@ -5,23 +5,47 @@ require 'nfg_csv_importer/file_origination_types/self_import_csv_xls'
 describe NfgCsvImporter::Onboarder::Steps::NavigationBarPresenter do
   let(:h) { NfgCsvImporter::Onboarding::ImportDataController.new.view_context }
   let(:navigation_bar_presenter) { described_class.new(onboarding_session, h) }
-  let(:onboarding_session) { NfgOnboarder::Session.new(name: 'import_data', current_step: current_step, step_data: step_data) }
-  let(:file_origination_type) { NfgCsvImporter::FileOriginationTypes::FileOriginationType.new(tested_type_sym, NfgCsvImporter::FileOriginationTypes::SelfImportCsvXls) }
-  let(:tested_type_sym) { :self_import_csv_xls }
+  let(:onboarding_session) { NfgOnboarder::Session.new(name: 'import_data', current_step: current_step) }
 
+  let(:steps) { [:first, :second, :last] }
+  let(:current_step) { :first }
 
-  let(:type_sym) { file_origination_type.type_sym }
+  before { h.expects(:controller).returns(stub(wizard_steps: steps)) }
 
-  let(:step_data) { { "import_data" => { file_origination_type_selection: ActionController::Parameters.new('file_origination_type' => type_sym) } } }
+  describe '#step_href(step, path)' do
+    let(:path) { 'path/to/step' }
 
-  it { expect(described_class).to be < NfgCsvImporter::Onboarder::OnboarderPresenter }
+    subject { navigation_bar_presenter.step_href(current_step, path) }
+
+    context 'when the supplied step is not the last step' do
+      let(:current_step) { :first }
+      it { is_expected.to eq path }
+    end
+
+    context 'when the supplied step is the last step' do
+      let(:current_step) { :last }
+      it { is_expected.to be_nil }
+    end
+  end
+
+  describe '#step_icon(step)' do
+    subject { navigation_bar_presenter.step_icon(current_step) }
+
+    context 'when step is the last step' do
+      let(:current_step) { :last }
+      it { is_expected.to eq 'check' }
+    end
+
+    context 'when step is not the last step' do
+      let(:current_step) { :first }
+      it { is_expected.to be_nil }
+    end
+  end
 
   describe '#show_nav?' do
     before do
-      h.expects(:controller).returns(stub(wizard_steps: steps))
       h.expects(:params).returns(id: current_step)
     end
-    let(:steps) { [:first, :second, :third] }
 
     subject { navigation_bar_presenter.show_nav? }
 
@@ -40,5 +64,17 @@ describe NfgCsvImporter::Onboarder::Steps::NavigationBarPresenter do
         expect(subject).to be_truthy
       end
     end
+  end
+
+  describe '#step_status(step)' do
+    # before { h.expects(:params).returns(id: current_step) }
+
+    # subject { navigation_bar_presenter.step_status(tested_step) }
+
+    # context 'when the tested step is the active step' do
+    #   let(:tested_step) { current_step }
+    #   it { is_expected.to eq :active }
+
+    # end
   end
 end
