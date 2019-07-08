d634535ec096a09de7c4484dc8ddca286cf888bd
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-07-08 15:48:53 -0400

    moves the data describe identifier via the filename from the progress bar to the previewElement. Updates spec.

--- spec/features/dropzone_interactions_spec.rb
+++ spec/features/dropzone_interactions_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-describe "Using the nfg_onboarder engine to import paypal transactions", js: true do
+describe "The dropzone drag/drop file uploader feature", js: true do
   let(:entity) { create(:entity) }
   let(:admin) {  create(:user) }
   let(:file_origination_type) { 'paypal' }
   let(:valid_filename) { 'paypal_sample_file.xlsx' }
+  let(:third_filename) { 'paypal_processed_file.csv' }
   let(:invalid_filename) { 'icon.jpg' }
 
-  it 'walks the user through selecting the paypal file and eventually imports the donors/donations in the file' do
-
+  it 'is a functioning dropzone drag/drop file uploader' do
     by 'visiting the index page' do
       visit nfg_csv_importer.imports_path
     end
@@ -34,10 +34,10 @@ describe "Using the nfg_onboarder engine to import paypal transactions", js: tru
     end
 
     and_it 'defaults to the empty state dropzone interface' do
-      expect(page).to have_selector '.dropzone-target', text: 'Drag and drop your'
+      expect(page).to have_selector '.dropzone-target.dz-clickable', text: 'Drag and drop your'
 
       and_it 'does not have the activated css classes enabled' do
-        # expect(page).not_to have_css '.dz-started.dz-drag-hover'
+        expect(page).not_to have_css '.dz-clickable.dz-started'
       end
     end
 
@@ -48,16 +48,24 @@ describe "Using the nfg_onboarder engine to import paypal transactions", js: tru
       end
 
       by 'dropping an image file' do
-        drop_in_dropzone(File.expand_path("spec/fixtures/icon.jpg"))
+        drop_in_dropzone(File.expand_path("spec/fixtures/#{invalid_filename}"))
       end
 
       # These css classes are specifically removed during the resetUI function.
       and_it 'activates the dropzone' do
-        # expect(page).to have_css '.dz-started.dz-drag-hover'
+        expect(page).to have_css '.dz-clickable.dz-started'
       end
 
       and_it 'renders the progress bar' do
         verify_progress_bar_completion(invalid_filename)
+      end
+
+      and_it 'renders the filename' do
+        expect(page).to have_selector "[data-dz-name]", text: invalid_filename
+      end
+
+      and_it 'renders the filesize' do
+        expect(page).to have_selector "[data-dz-size]", text: '24.5 KB'
       end
 
       and_it 'puts the dropzone file field in an error state' do
@@ -77,6 +85,7 @@ describe "Using the nfg_onboarder engine to import paypal transactions", js: tru
       end
 
       and_it 'is removable from the dropzone interface' do
+        expect(page).to have_css '.dz-remove'
         click_link 'Remove file'
       end
 
@@ -85,18 +94,78 @@ describe "Using the nfg_onboarder engine to import paypal transactions", js: tru
       end
 
       and_it 'returns to the standard presentation once the file is removed' do
-        # expect(page).not_to have_css '.dz-started.dz-drag-hover'
+        expect(page).not_to have_css '.dz-clickable.dz-started'
         expect(page).to have_selector '.dropzone-target', text: 'Drag and drop your'
       end
     end
 
-    and_by 'attaching the a paypal file to the dropzone file field' do
-      drop_in_dropzone(File.expand_path("spec/fixtures/paypal_sample_file.xlsx"))
-      verify_progress_bar_completion(valid_filename)
+    and_context 'attaching the a (valid file) paypal file to the dropzone file field' do
+      drop_in_dropzone(File.expand_path("spec/fixtures/#{valid_filename}"))
+      and_it 'confirms the upload of the file' do
+        verify_progress_bar_completion(valid_filename)
+      end
+
+      and_it 'does not display any error messages' do
+        expect(page).not_to have_css "[data-dz-errormessage]"
+      end
+
+      and_it 'does not show an error icon' do
+        expect(page).not_to have_css '.dz-error-mark'
+      end
+
+      and_it 'includes a remove file link' do
+        expect(page).to have_selector '.dz-remove', text: 'Remove file'
+      end
+
+      and_it 'renders the filename' do
+        expect(page).to have_selector "[data-dz-name]", text: valid_filename
+      end
+
+      and_it 'renders the filesize' do
+        expect(page).to have_selector "[data-dz-size]", text: '14.1 KB'
+      end
+
+      and_it 'shows the success icon' do
+        expect(page).to have_css '.dz-success-mark .fa-check'
+      end
+
+      and_it 'does not put the dropzone file field in an error state' do
+        expect(page).not_to have_css '.dz-error.dz-complete'
+      end
+    end
+
+    and_context 'adding a second file' do
+      and_it 'adds the second file to the dropzone interface' do
+        drop_in_dropzone(File.expand_path("spec/fixtures/#{third_filename}"))
+      end
+
+      and_it 'shows the file was uploaded' do
+        verify_progress_bar_completion(third_filename)
+      end
+
+      and_it 'includes both files on the page' do
+        verify_progress_bar_completion(valid_filename)
+      end
+
+      and_it 'removes the file' do
+        page.find("[data-describe='dz-#{third_filename}'] .dz-remove", text: 'Remove file').click
+      end
+
+      and_it 'is removed' do
+        expect(page).not_to have_css "[data-describe='dz-#{third_filename}']"
+      end
+    end
+
+    and_it 'submits the form' do
+      click_button 'Next'
+    end
+
+    and_it 'takes you to the correct next step' do
+      expect(page).to have_css 'body.nfg_csv_importer-onboarding-import_data.preview_confirmation.paypal'
     end
   end
 end
 
 def verify_progress_bar_completion(filename)
-  expect(page.find("[data-describe='progress-bar-for-#{filename}'].progress-bar[style='width: 100%;']")).to be
+  expect(page.find("[data-describe='dz-#{filename}'] .progress-bar[style='width: 100%;']")).to be
 end
