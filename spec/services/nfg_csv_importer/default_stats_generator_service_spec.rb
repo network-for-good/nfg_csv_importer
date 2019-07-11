d3b71df0dacf54a13cbfdfaf75bfdbb835560696
Thomas Hoen <tom@givecorps.com>
2019-07-11 11:06:26 -0400

    [#DM-5291_rails_5_automagical_imports_for_DMS] Corrects the row count and randomization of the rows in the default generator spec

--- spec/services/nfg_csv_importer/default_stats_generator_service_spec.rb
+++ spec/services/nfg_csv_importer/default_stats_generator_service_spec.rb
@@ -11,15 +11,16 @@ RSpec.describe NfgCsvImporter::DefaultStatsGeneratorService do
   let(:data) { 'some-data' }
   let(:another_data) { 'another-data' }
   let(:row_conversion_method) { -> (i) { spreadsheet[1] } }
+  let(:number_of_rows) { spreadsheet.length - 1 } # we don't count the first row, which holds the header info
   let(:response) do
     {
-      "summary_data" => { "number_of_rows" => spreadsheet.length },
+      "summary_data" => { "number_of_rows" => number_of_rows },
       "example_rows" => [ data, another_data ]
     }
   end
 
   before do
-    spreadsheet.expects(:last_row).at_least(1).returns(spreadsheet.length + 1)
+    spreadsheet.expects(:last_row).at_least(1).returns(spreadsheet.length)
     # Random.expects(:new).returns(mock(rand: 7))
   end
 
@@ -39,7 +40,7 @@ RSpec.describe NfgCsvImporter::DefaultStatsGeneratorService do
       let(:row_conversion_method) { nil }
       let(:response) do
         {
-          "summary_data" => { "number_of_rows" => 10 },
+          "summary_data" => { "number_of_rows" => number_of_rows },
           "example_rows" => [ nil, nil ]
         }
       end
