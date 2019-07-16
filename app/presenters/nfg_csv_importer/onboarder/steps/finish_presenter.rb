6f47269a6aed3eab01ad952c69bbb325e23c5453
Andrew Porterfield <ajporterfield@gmail.com>
2019-07-16 11:37:28 -0400

    Improved queue alert message

--- app/presenters/nfg_csv_importer/onboarder/steps/finish_presenter.rb
+++ app/presenters/nfg_csv_importer/onboarder/steps/finish_presenter.rb
@@ -2,6 +2,19 @@ module NfgCsvImporter
   module Onboarder
     module Steps
       class FinishPresenter < NfgCsvImporter::Onboarder::OnboarderPresenter
+        def queued_alert_msg
+          if queue_index == 0
+            "Your import is next in line."
+          else
+            "There #{queue_index == 1 ? 'is' : 'are'} #{h.pluralize(queue_index, 'import')} in line ahead of yours."
+          end
+        end
+
+        private
+
+        def queue_index
+          @queue_index ||= NfgCsvImporter::Import.queued.pluck(:id).index(h.import.id) || 0
+        end
       end
     end
   end
