66117beb0b4e126f8a49a0e9f5052893b1d38d12
Thomas Hoen <tom@givecorps.com>
2019-07-19 13:08:50 -0400

    [#DM-5291_rails_5_automagical_imports_for_DMS] remove duplicate methods

--- app/controllers/nfg_csv_importer/application_controller.rb
+++ app/controllers/nfg_csv_importer/application_controller.rb
@@ -3,20 +3,5 @@ module NfgCsvImporter
     include NfgCsvImporter::Concerns::ImportAttributeLoaders
     helper NfgCsvImporter::ImportsHelper
     helper NfgUi::ApplicationHelper
-
-  private
-
-    def load_imported_by
-      @imported_by ||= self.send("current_#{NfgCsvImporter.configuration.imported_by_class.downcase}")
-    end
-
-    def load_imported_for
-      @imported_for ||= self.send "#{NfgCsvImporter.configuration.imported_for_class.downcase}".to_sym
-    end
-
-    def load_import
-      @import = @imported_for.imports.find(params[:id] || params[:import_id])
-    end
-
   end
 end
