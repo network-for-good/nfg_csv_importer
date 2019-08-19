ab012f7589177d7746c058f9b655d9d634622331
Timothy King <tmk@lordzork.com>
2019-08-19 16:22:11 -0400

    Adds default_url_options to test_app production.rb

--- spec/test_app/config/environments/production.rb
+++ spec/test_app/config/environments/production.rb
@@ -75,4 +75,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  default_url_options = { :host => 'example.com', :port => 80, protocol: "http" }
 end
