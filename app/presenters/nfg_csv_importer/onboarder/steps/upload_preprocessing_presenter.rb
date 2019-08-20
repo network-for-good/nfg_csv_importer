3f485f43e63183f5655d67c33c5a9740342e60e1
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-08-20 09:01:42 -0400

    Adds variable language for the upload_preprocessing step & specs.

--- app/presenters/nfg_csv_importer/onboarder/steps/upload_preprocessing_presenter.rb
+++ app/presenters/nfg_csv_importer/onboarder/steps/upload_preprocessing_presenter.rb
@@ -2,6 +2,30 @@ module NfgCsvImporter
   module Onboarder
     module Steps
       class UploadPreprocessingPresenter < NfgCsvImporter::Onboarder::OnboarderPresenter
+        # To take advantage of custom header page text on this step
+        #
+        # Add a new entry based on the file_origination_type type_sym  to your yml file like this:
+        #
+        # on: config/locales/views/onboarding/import_data.yml
+        #
+        # import_data:
+        #   upload_preprocessing:
+        #     header:
+        #       file_origination_type:
+        #         paypal: "custom header..."
+        #         mailchimp: "etc..."
+        def header_page_text
+          scope = [:nfg_csv_importer, :onboarding, :import_data, :upload_preprocessing]
+
+          I18n.t(".header.file_origination_type.#{file_origination_type.type_sym}",
+                  scope: scope,
+
+                  # Fallback in the event a custom header hasn't been set.
+                  default: I18n.t('.header.page',
+                                  scope: scope,
+                                  file_origination_type: file_origination_type.name,
+                                  description_of_files: "they come in a 'package' and look like this: some_filename.csv"))
+        end
       end
     end
   end
--- spec/presenters/nfg_csv_importer/onboarder/steps/upload_preprocessing_presenter_spec.rb
+++ spec/presenters/nfg_csv_importer/onboarder/steps/upload_preprocessing_presenter_spec.rb
@@ -1,19 +1,36 @@
 require "rails_helper"
-# include Rails.application.routes.url_helpers
-require 'nfg_csv_importer/file_origination_types/self_import_csv_xls'
 
 describe NfgCsvImporter::Onboarder::Steps::UploadPreprocessingPresenter do
-  let(:h) { NfgCsvImporter::Onboarding::ImportDataController.new.view_context }
-  let(:upload_preprocessing_presenter) { described_class.new(onboarding_session, h) }
-  let(:onboarding_session) { NfgOnboarder::Session.new(name: 'import_data', current_step: current_step, step_data: step_data) }
-  let(:file_origination_type) { NfgCsvImporter::FileOriginationTypes::FileOriginationType.new(tested_type_sym, FileOriginationTypes::SelfImportCsvXls) }
-  let(:tested_type_sym) { :self_import_csv_xls }
-
+    let(:h) { NfgCsvImporter::Onboarding::ImportDataController.new.view_context }
+  let(:onboarding_session) { FactoryGirl.create(:onboarding_session, *traits, current_step: current_step) }
+  let(:traits) { [] }
   let(:current_step) { 'upload_preprocessing' }
+  let(:onboarder_presenter) { NfgCsvImporter::Onboarder::OnboarderPresenter.build(onboarding_session, h) }
 
-  let(:type_sym) { file_origination_type.type_sym }
-
-  let(:step_data) { { "import_data" => { file_origination_type_selection: ActionController::Parameters.new('file_origination_type' => type_sym) } } }
+  before { h.controller.stubs(:params).returns(id: current_step) }
 
   it { expect(described_class).to be < NfgCsvImporter::Onboarder::OnboarderPresenter }
+
+  describe '#header_page_text' do
+    let(:scope) { [:nfg_csv_importer, :onboarding, :import_data, :upload_preprocessing] }
+    subject { onboarder_presenter.header_page_text }
+
+    context 'when the header text is for an existing file_origination_type' do
+      let(:traits) { [:paypal_file_origination_type] }
+      it 'renders the paypal pre-formatted header text using the file origination type type_sym attribute' do
+        expect(subject).to eq I18n.t(".header.file_origination_type.paypal", scope: scope)
+      end
+    end
+
+    context 'when the file origination type does not have a custom header text' do
+      let(:traits) { [:self_import_csv_xls_file_origination_type] }
+
+      it 'renders the default language' do
+        expect(subject).to eq I18n.t('.header.page',
+                                  scope: scope,
+                                  file_origination_type: "Your own spreadsheet",
+                                  description_of_files: "they come in a 'package' and look like this: some_filename.csv")
+      end
+    end
+  end
 end
