8dad1f6e15677e74dbc46149ce5f295d6e341d61
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-08-20 13:14:36 -0400

    Dynamicizes the header message and header text. Also hooks up an optional below-the-form area which we're using for an alert.

--- app/presenters/nfg_csv_importer/onboarder/steps/upload_preprocessing_presenter.rb
+++ app/presenters/nfg_csv_importer/onboarder/steps/upload_preprocessing_presenter.rb
@@ -12,19 +12,40 @@ module NfgCsvImporter
         #   upload_preprocessing:
         #     header:
         #       file_origination_type:
-        #         paypal: "custom header..."
-        #         mailchimp: "etc..."
+        #         page:
+        #           paypal: "custom header..."
+        #           mailchimp: "etc..."
         def header_page_text
-          scope = [:nfg_csv_importer, :onboarding, :import_data, :upload_preprocessing]
-
-          I18n.t(".header.file_origination_type.#{file_origination_type.type_sym}",
-                  scope: scope,
+          I18n.t(".header.file_origination_type.page.#{file_origination_type.type_sym}",
+                  scope: i18n_scope,
 
                   # Fallback in the event a custom header hasn't been set.
                   default: I18n.t('.header.page',
-                                  scope: scope,
+                                  scope: i18n_scope,
                                   file_origination_type: file_origination_type.name,
                                   description_of_files: "they come in a 'package' and look like this: some_filename.csv"))
+        end
+
+        # import_data:
+        #   upload_preprocessing:
+        #     header:
+        #       file_origination_type:
+        #         message:
+        #           paypal: "custom message text..."
+        #           mailchimp: "custom message..."
+        def header_message_text
+          I18n.t(".header.file_origination_type.message.#{file_origination_type.type_sym}",
+                  scope: i18n_scope,
+
+                  # fallback
+                  default: I18n.t('.header.message', file_origination_type: file_origination_type.name, scope: i18n_scope)
+                  )
+        end
+
+        private
+
+        def i18n_scope
+          [:nfg_csv_importer, :onboarding, :import_data, :upload_preprocessing]
         end
       end
     end
--- spec/presenters/nfg_csv_importer/onboarder/steps/upload_preprocessing_presenter_spec.rb
+++ spec/presenters/nfg_csv_importer/onboarder/steps/upload_preprocessing_presenter_spec.rb
@@ -6,19 +6,18 @@ describe NfgCsvImporter::Onboarder::Steps::UploadPreprocessingPresenter do
   let(:traits) { [] }
   let(:current_step) { 'upload_preprocessing' }
   let(:onboarder_presenter) { NfgCsvImporter::Onboarder::OnboarderPresenter.build(onboarding_session, h) }
+  let(:scope) { [:nfg_csv_importer, :onboarding, :import_data, :upload_preprocessing] }
 
   before { h.controller.stubs(:params).returns(id: current_step) }
 
   it { expect(described_class).to be < NfgCsvImporter::Onboarder::OnboarderPresenter }
 
   describe '#header_page_text' do
-    let(:scope) { [:nfg_csv_importer, :onboarding, :import_data, :upload_preprocessing] }
     subject { onboarder_presenter.header_page_text }
-
     context 'when the header text is for an existing file_origination_type' do
       let(:traits) { [:paypal_file_origination_type] }
       it 'renders the paypal pre-formatted header text using the file origination type type_sym attribute' do
-        expect(subject).to eq I18n.t(".header.file_origination_type.paypal", scope: scope)
+        expect(subject).to eq I18n.t(".header.file_origination_type.page.paypal", scope: scope)
       end
     end
 
@@ -30,6 +29,24 @@ describe NfgCsvImporter::Onboarder::Steps::UploadPreprocessingPresenter do
                                   scope: scope,
                                   file_origination_type: "Your own spreadsheet",
                                   description_of_files: "they come in a 'package' and look like this: some_filename.csv")
+      end
+    end
+  end
+
+  describe '#header_message_text' do
+    subject { onboarder_presenter.header_message_text }
+    context 'when the file origination type does not have a custom header message' do
+      let(:traits) { [:paypal_file_origination_type] }
+      it 'renders the correct header message text usingn the default fallback' do
+        expect(subject).to eq I18n.t('.header.message', file_origination_type: 'PayPal', scope: scope)
+      end
+    end
+
+    context 'when the file origination type does not have a custom header text' do
+      let(:traits) { [:send_to_nfg_file_origination_type] }
+
+      it 'renders the default language' do
+        expect(subject).to eq I18n.t('.header.file_origination_type.message.send_to_nfg', scope: scope)
       end
     end
   end
