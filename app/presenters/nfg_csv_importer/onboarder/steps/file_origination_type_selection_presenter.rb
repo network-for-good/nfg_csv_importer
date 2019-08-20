3f07d1127a39c0c50c09fccd26eec64c9f57735e
Jonathan Roehm <jonathan.roehm@networkforgood.com>
2019-08-20 05:51:57 -0400

    Removes selective name-showing on the file_origination_type_selection step -- names now show as a title under the image for all FOTs.

--- app/presenters/nfg_csv_importer/onboarder/steps/file_origination_type_selection_presenter.rb
+++ app/presenters/nfg_csv_importer/onboarder/steps/file_origination_type_selection_presenter.rb
@@ -2,22 +2,6 @@ module NfgCsvImporter
   module Onboarder
     module Steps
       class FileOriginationTypeSelectionPresenter < NfgCsvImporter::Onboarder::OnboarderPresenter
-        # Shows or hides the file origiation type's #name
-        # based on a manually generated blacklist
-        #
-        # This should probably be set on the host app, for now this manually
-        # turns on or off the file origination type's #name as a string
-        # on the file origination type radio button partial:
-        # app/views/nfg_csv_importer/onboarding/import_data/file_origination_type_selection/_file_origination_type_radio_button.html.haml
-        def show_file_origination_type_name?(type_sym = '')
-          file_origination_types_that_require_names_to_be_visible.include?(type_sym.to_s)
-        end
-
-        private
-
-        def file_origination_types_that_require_names_to_be_visible
-          %w[self_import_csv_xls send_to_nfg]
-        end
       end
     end
   end
--- spec/presenters/nfg_csv_importer/onboarder/steps/file_origination_type_selection_presenter_spec.rb
+++ spec/presenters/nfg_csv_importer/onboarder/steps/file_origination_type_selection_presenter_spec.rb
@@ -10,19 +10,4 @@ describe NfgCsvImporter::Onboarder::Steps::FileOriginationTypeSelectionPresenter
   before { h.controller.stubs(:params).returns(id: current_step) }
 
   it { expect(described_class).to be < NfgCsvImporter::Onboarder::OnboarderPresenter }
-
-  describe '#show_file_origination_type_name?(type_sym = '')' do
-    let(:file_origination_types_that_require_names_to_be_visible) { %w[self_import_csv_xls send_to_nfg] }
-    subject { onboarder_presenter.show_file_origination_type_name?(type_sym) }
-
-    context 'when the file origination type name is included on the list of names required' do
-      let(:type_sym) { :self_import_csv_xls }
-      it { is_expected.to be }
-    end
-
-    context 'when the file origination type name is *not* included on the list of names required' do
-      let(:type_sym) { :paypal }
-      it { is_expected.not_to be }
-    end
-  end
 end
