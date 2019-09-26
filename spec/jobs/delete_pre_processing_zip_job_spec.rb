6cd00b07bcf37c89b6a8fd718f23a22526e925f9
Harsh <pathakh@msn.com>
2019-09-26 14:13:44 -0400

    fix broken spec by changing guard minutes, and adding another spec for guard mintues

--- spec/jobs/delete_pre_processing_zip_job_spec.rb
+++ spec/jobs/delete_pre_processing_zip_job_spec.rb
@@ -3,13 +3,21 @@ require 'rails_helper'
 describe NfgCsvImporter::DeleteZipJob do
   let!(:delete_zip_job) { NfgCsvImporter::DeleteZipJob.new }
   let!(:folder) { 'tmp/some_folder' }
-
-  subject { delete_zip_job.perform(folder) }
+  let(:minutes) { 0 }
+  subject { delete_zip_job.perform(folder, minutes) }
 
   before { FileUtils.mkdir_p(folder) unless Dir.exists?(folder) }
 
   it 'deletes the folder' do
     expect{ subject }.to change{ Dir.exists?(folder) }.from(true).to(false)
+  end
+
+  context 'when file was created more recent than the guard minutes' do
+    let(:minutes) { 10 }
+
+    it 'does not delete the folder' do
+      expect{ subject }.to_not change{ Dir.exists?(folder) }
+    end
   end
 
 end
