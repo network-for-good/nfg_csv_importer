b213527a893a23f596802c0aba65fe325e0157f3
Harsh <pathakh@msn.com>
2019-11-15 17:14:35 -0500

    fix import index page stuck in continue your import

--- spec/features/uploading_your_own_spreadsheet_spec.rb
+++ spec/features/uploading_your_own_spreadsheet_spec.rb
@@ -13,27 +13,7 @@ describe "Importing your own spreadsheet", js: true do
   end
 
   it 'Walks the admin through uploading/mapping/importing their own spreadsheet' do
-    by 'initiating an import' do
-      visit nfg_csv_importer.imports_path
-      page.find("[data-describe='import-data-onboarder-cta']").click
-    end
-
-    and_by 'selecting the "import your own" file origination type' do
-      # file_origination_type_selection
-      page.find("label[for='nfg_csv_importer_onboarding_import_data_file_origination_type_selection_file_origination_type_self_import_csv_xls']").click
-      click_next_button_for('file_origination_type_selection')
-    end
-
-
-    and_by 'selecting the user import type' do
-      # import type page (visible since there are more than one import type available for this user)
-      page.find("label[for='nfg_csv_importer_onboarding_import_data_import_type_import_type_users']").click
-
-      # we save the import record when the user selects the import type
-      expect do
-        click_next_button_for('import_type')
-      end.to change(NfgCsvImporter::Import, :count).by(1)
-    end
+    navigating_till_user_import_type
 
     and_by 'exiting' do
       click_link('Exit')
@@ -49,55 +29,7 @@ describe "Importing your own spreadsheet", js: true do
 
     import = NfgCsvImporter::Import.last
 
-    and_by 'reviewing the import overview page' do
-      click_next_button_for('overview')
-    end
-
-    and_by 'uploading the post procesing import files' do
-      # upload_post_processing
-      attach_file 'nfg_csv_importer_onboarding_import_data_upload_post_processing_import_file', path_to_file, visible: false
-      click_next_button_for('upload_post_processing')
-    end
-
-    and_by 'mapping the fields' do
-      # field_mapping
-      within_frame('edit_import_iframe') do
-        within 'div[data-column-name="last_name"]' do
-          click_link 'Edit Column'
-        end
-        expect(page).to have_text "Not ready to import"
-      end
-
-      expect(page).to have_button(next_button_label('field_mapping'), disabled: true)
-      within_frame('edit_import_iframe') do
-        within('#card_header_last_name') do
-          select 'Last Name', from: 'import_fields_mapping_bGFzdF9uYW1l_'
-        end
-        expect(page).to have_text "Ready to import"
-      end
-      click_button 'Next'
-    end
-
-    and_by 'reviewing the details and summary of the file and submitting the import' do
-      # preview_confirmation
-      expect(page).to have_content("TOTAL ROWS 3")
-
-      expect do
-        click_next_button_for('preview_confirmation')
-        page.driver.browser.switch_to.alert.accept
-
-        # We now should be headed to the finish page
-        # but let's wait here until the import completes
-        while import.reload.status != 'complete' do
-          # the import is running, we just need to wait until it completes
-          sleep 0.05
-        end
-      end.to change(User, :count).by(2)
-
-      # and_it 'shows the column mapping conversion table' do
-      #   expect(page).to have_css "[data-describe='column-mappings']"
-      # end
-    end
+    navigating_from_overview_to_finish(import: import)
 
     and_by "finishing the import process by leaving the onboarder" do
       # finish
@@ -118,6 +50,35 @@ describe "Importing your own spreadsheet", js: true do
 
     and_it 'shows column mapping information' do
       expect(page).to have_css "[data-describe='column-mappings']"
+    end
+  end
+
+  context 'when the import is marked as deleting' do
+    it 'takes the user through a new import' do
+      navigating_till_user_import_type
+      import = NfgCsvImporter::Import.last
+      navigating_from_overview_to_finish(import: import)
+
+      and_by "finishing the import process by leaving the onboarder using direct page refresh" do
+        # finish
+        expect(page).to have_text "You've finished this import"
+        visit nfg_csv_importer.imports_path
+
+        # back to the import list
+        expect(page.current_path).to eq '/imports/'
+      end
+
+      and_by 'deleting the import' do
+        page.find("[data-describe='import_dropdown-#{import.id}']").click
+        click_link I18n.t('nfg_csv_importer.imports.index.links.delete_import')
+        page.driver.browser.switch_to.alert.accept
+        expect(page).to have_content I18n.t('nfg_csv_importer.imports.index.status.deleted')
+        expect(page).to have_content I18n.t('nfg_csv_importer.imports.index.buttons.begin')
+        click_link I18n.t('nfg_csv_importer.imports.index.buttons.begin')
+        and_it 'takes you back to file origination type selection page' do
+          expect(page.current_path).to eq '/imports/onboarding/import_data/file_origination_type_selection'
+        end
+      end
     end
   end
 end
