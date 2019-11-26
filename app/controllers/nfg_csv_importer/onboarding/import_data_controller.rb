6d624c49059d800ebc304f92ac8b10748ef52252
Harsh <pathakh@msn.com>
2019-11-26 13:29:03 -0500

    Fix specs

--- app/controllers/nfg_csv_importer/onboarding/import_data_controller.rb
+++ app/controllers/nfg_csv_importer/onboarding/import_data_controller.rb
@@ -61,6 +61,7 @@ module NfgCsvImporter
         return unless import.uploaded_or_calculating_statistics? # only when the import is still in an 'uploaded' state should we attempt to enqueue it
         import.queued!
         NfgCsvImporter::ImportMailer.send_import_result(import).deliver_now
+        import.update_attribute(:imported_by, imported_by)
         NfgCsvImporter::ProcessImportJob.perform_later(import.id)
       end
 
