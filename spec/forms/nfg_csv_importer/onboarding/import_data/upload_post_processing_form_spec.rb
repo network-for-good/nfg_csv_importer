6aef5ff9985b0d77cb42a87d0e45ea3e9b170ea7
Timothy King <tmk@lordzork.com>
2022-08-16 12:39:48 -0400

    Fixes spec

--- spec/forms/nfg_csv_importer/onboarding/import_data/upload_post_processing_form_spec.rb
+++ spec/forms/nfg_csv_importer/onboarding/import_data/upload_post_processing_form_spec.rb
@@ -44,7 +44,7 @@ describe NfgCsvImporter::Onboarding::ImportData::UploadPostProcessingForm  do
 
     it "should add errors to base" do
       subject
-      expect(import_file_validateable_host.errors.messages[:base].first).to be_nil
+      expect(import_file_validateable_host.errors.messages[:base]).to be_nil
     end
   end
 end
