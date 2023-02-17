ae7641fa71287fa8125b0d2e42eb5a0aa326eebb
Aravind <aravind@clearstack.io>
2023-02-17 12:26:54 +0530

    adds comments

--- app/presenters/nfg_csv_importer/onboarder/steps/file_origination_type_selection_presenter.rb
+++ app/presenters/nfg_csv_importer/onboarder/steps/file_origination_type_selection_presenter.rb
@@ -23,6 +23,13 @@ module NfgCsvImporter
           I18n.t("nfg_csv_importer.onboarding.import_data.file_origination_type_selection.file_origination_type_title.#{file_origination_type.type_sym}", default: file_origination_type.name)
         end
 
+        #
+        # Used add a custom file_origination_type description to the file origination type radio buttons.
+        # The default file description content contains "contacts and donations" but it does not make sense
+        # for all enclosing applications, for example in the case of auctions the bidder import description
+        # hould contain "bidders" instead of "donations" or "contacts".
+        # It falls back to the file_origination_type.description if no custom description is present.
+        #
         def file_origination_type_description(file_origination_type:)
           I18n.t("nfg_csv_importer.onboarding.import_data.file_origination_type_selection.file_origination_type_description.#{file_origination_type.type_sym}", default: file_origination_type.description)
         end
