3df2e1cd4b67b506e6988b0d6348208902ec64da
Pavan Kumar K C <kcpavan@me.com>
2023-03-28 08:46:47 +0530

    Spec fixes

---
+++ spec/test_app/app/assets/config/manifest.js
@@ -0,0 +1,7 @@
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+
+//= link nfg_csv_importer/email/application.css
+//= link nfg_ui/app_icon/site.webmanifest
+//= link nfg_ui/app_icon/browserconfig.xml
