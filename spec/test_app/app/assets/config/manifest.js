390d0c3613ebb4313ff691db027fe25db0c3d289
Pavan Kumar K C <kcpavan@me.com>
2023-03-28 12:53:26 +0530

    Fix nfg_csv_importer manifest.js

--- spec/test_app/app/assets/config/manifest.js
+++ spec/test_app/app/assets/config/manifest.js
@@ -2,6 +2,6 @@
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
 
-//= link nfg_csv_importer/email/application.css
+//= link nfg_csv_importer_manifest
 //= link nfg_ui/app_icon/site.webmanifest
 //= link nfg_ui/app_icon/browserconfig.xml
