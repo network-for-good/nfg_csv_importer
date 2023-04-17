1deed8951f820c0c01618584705ff1a0ad6ff757
Tom Hoen <tom@givecorps.com>
2023-04-17 10:52:56 -0400

    Update the Rails version in the test app to 6.1

--- spec/test_app/config/application.rb
+++ spec/test_app/config/application.rb
@@ -19,6 +19,9 @@ module TestApp
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # load all of the configuration defaults up to and including Rails 6.1
+    config.load_defaults 6.1
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
