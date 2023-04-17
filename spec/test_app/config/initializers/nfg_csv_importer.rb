075e5bf8cdc481dcf642a69c5504d8cedf73686f
Timothy King <tmk@lordzork.com>
2023-04-17 14:41:14 -0400

    Decreases uniqueness time to 20 minutes in test app

--- spec/test_app/config/initializers/nfg_csv_importer.rb
+++ spec/test_app/config/initializers/nfg_csv_importer.rb
@@ -9,6 +9,6 @@ NfgCsvImporter.configure do |config|
   config.disable_import_initiation_message = -> (user) { user.last_name == 'noob' ? "You can't see it" : nil }
   config.max_number_of_rows_allowed = 50000
   config.high_priority_queue_name = :my_fine_queue
-  config.process_import_job_sidekiq_options = { unique_for: 30.minutes }
+  config.process_import_job_sidekiq_options = { unique_for: 20.minutes }
   config.destroy_import_job_sidekiq_options = { backtrace: true }
 end
