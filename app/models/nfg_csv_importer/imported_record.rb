f78b3543f117ed9b6620015e2c327d52a138faf7
James Moline <jmoline@salsalabs.com>
2023-10-26 17:33:20 -0400

    AIO-2598 updating relationships

--- app/models/nfg_csv_importer/imported_record.rb
+++ app/models/nfg_csv_importer/imported_record.rb
@@ -3,7 +3,7 @@ class NfgCsvImporter::ImportedRecord < ActiveRecord::Base
 
   serialize :row_data, Hash
 
-  belongs_to :import
+  belongs_to :import, optional: true
   belongs_to :imported_by, class_name: NfgCsvImporter.configuration.imported_by_class, foreign_key: :imported_by_id
   belongs_to :imported_for, class_name: NfgCsvImporter.configuration.imported_for_class, foreign_key: :imported_for_id
   belongs_to :importable, polymorphic: true, dependent: :destroy
