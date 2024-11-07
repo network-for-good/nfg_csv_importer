3154fd65c760804b1177ba0db011654eeaf4e408
Ganesh Mohan <ganesh.mohan@networkforgood.com>
2024-11-07 21:54:57 +0530

    [#NFG-781] spec fix

--- spec/views/onboarding/import_data/import_type.html.haml_spec.rb
+++ spec/views/onboarding/import_data/import_type.html.haml_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe "onboarding/import_data/import_type.html.haml", type: :view do
   before do
     stub_template "nfg_csv_importer/onboarding/_sub_layout.html.haml" => "= yield"
-    allow(view).to receive(:f).and_return(double("FormBuilder", radio_button: true))
-    allow(view).to receive(:ui).and_return(double("UIHelper", nfg: nil))
+    view.stubs(:f).returns(stub("FormBuilder", radio_button: true))
+    view.stubs(:ui).returns(stub("UIHelper", nfg: nil))
   end
 
   context "when definition.import_title is nil" do
@@ -12,12 +12,12 @@ RSpec.describe "onboarding/import_data/import_type.html.haml", type: :view do
     let(:definition) { OpenStruct.new(import_title: nil, headline: "Example Headline") }
 
     before do
-      allow(view).to receive(:import_definitions).and_return({ example_import_type: definition })
+      view.stubs(:import_definitions).returns({ example_import_type: definition })
     end
 
     it "calls ui.nfg with the title as the pluralized and titleized import_type" do
+      view.ui.expects(:nfg).with(has_entry(title: import_type.to_s.pluralize.titleize))
       render
-      expect(view.ui).to have_received(:nfg).with(hash_including(title: import_type.to_s.pluralize.titleize))
     end
   end
 
@@ -26,12 +26,12 @@ RSpec.describe "onboarding/import_data/import_type.html.haml", type: :view do
     let(:definition) { OpenStruct.new(import_title: import_title, headline: "Example Headline") }
 
     before do
-      allow(view).to receive(:import_definitions).and_return({ example_import_type: definition })
+      view.stubs(:import_definitions).returns({ example_import_type: definition })
     end
 
     it "calls ui.nfg with the title as definition.import_title" do
+      view.ui.expects(:nfg).with(has_entry(title: import_title))
       render
-      expect(view.ui).to have_received(:nfg).with(hash_including(title: import_title))
     end
   end
 end
