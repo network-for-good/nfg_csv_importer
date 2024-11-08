c8659a19d7183dda79b79008869c322e9d929422
Ganesh Mohan <ganesh.mohan@networkforgood.com>
2024-11-08 22:46:54 +0530

    [#NFG-781] spec fix

--- spec/views/onboarding/import_data/import_type.html.haml_spec.rb
+++ spec/views/onboarding/import_data/import_type.html.haml_spec.rb
@@ -2,9 +2,12 @@ require "rails_helper"
 
 RSpec.describe "onboarding/import_data/import_type.html.haml", type: :view do
   before do
+    engine_root = NfgCsvImporter::Engine.root
+    view.lookup_context.view_paths.push(engine_root.join('app', 'views'))
     view.stubs(:f).returns(stub("FormBuilder", radio_button: ''))
     view.stubs(:t).returns('')  # Stub translation helper
     view.stubs(:onboarder_presenter).returns(stub('Presenter', render_google_tag_manager: ''))
+    view.stubs(:ui).returns(stub('UIHelper', nfg: '<nfg output>'))
   end
 
   context "when definition.import_title is nil" do
@@ -12,7 +15,7 @@ RSpec.describe "onboarding/import_data/import_type.html.haml", type: :view do
     let(:definition) { OpenStruct.new(import_title: nil, headline: "Example Headline") }
 
     before do
-      assign(:import_definitions, { example_import_type => definition })
+      assign(:import_definitions, { import_type => definition })
     end
 
     it "displays the pluralized and titleized import_type as the title" do
@@ -22,11 +25,12 @@ RSpec.describe "onboarding/import_data/import_type.html.haml", type: :view do
   end
 
   context "when definition.import_title has a value" do
+    let(:import_type) { "example_import_type" }
     let(:import_title) { "Custom Import Title" }
     let(:definition) { OpenStruct.new(import_title: import_title, headline: "Example Headline") }
 
     before do
-      assign(:import_definitions, { example_import_type: definition })
+      assign(:import_definitions, { import_type => definition })
     end
 
     it "displays definition.import_title as the title" do
