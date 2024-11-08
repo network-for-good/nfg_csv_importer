e00b62449ba0e5bd121e2f0fd6d8f6aa8be733db
Ganesh Mohan <ganesh.mohan@networkforgood.com>
2024-11-08 20:39:32 +0530

    [#NFG-781] spec fix

--- spec/views/onboarding/import_data/import_type.html.haml_spec.rb
+++ spec/views/onboarding/import_data/import_type.html.haml_spec.rb
@@ -19,8 +19,9 @@ RSpec.describe "onboarding/import_data/import_type.html.haml", type: :view do
     end
 
     it "calls ui.nfg with the title as the pluralized and titleized import_type" do
-      expect(@mock_ui).to receive(:nfg).with(has_entry(title: "Example Import Types"))
+      allow(@mock_ui).to receive(:nfg).with(has_entry(title: "Example Import Types"))
       render
+      expect(@mock_ui).to have_received(:nfg).with(has_entry(title: "Example Import Types"))
     end
   end
 
@@ -35,8 +36,9 @@ RSpec.describe "onboarding/import_data/import_type.html.haml", type: :view do
     end
 
     it "calls ui.nfg with the title as definition.import_title" do
-      expect(@mock_ui).to receive(:nfg).with(has_entry(title: "Custom Import Title"))
+      allow(@mock_ui).to receive(:nfg).with(has_entry(title: "Custom Import Title"))
       render
+      expect(@mock_ui).to have_received(:nfg).with(has_entry(title: "Custom Import Title"))
     end
   end
 end
