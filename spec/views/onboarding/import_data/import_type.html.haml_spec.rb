832cfb9fff1443d659fa9549c559c6b714ea0125
Ganesh Mohan <ganesh.mohan@networkforgood.com>
2024-11-08 23:17:05 +0530

    [#NFG-781] spec fix

--- spec/views/onboarding/import_data/import_type.html.haml_spec.rb
+++ spec/views/onboarding/import_data/import_type.html.haml_spec.rb
@@ -1,33 +1,31 @@
 require "rails_helper"
 
-def fake_form
-  # the sublayout needs to supply a form that is passed
-  # to its children. The text block below returns
-  # a fake version of the the sublayout which requires
-  # a lot more setup than is healthy for a simple view
-  # spec. So we return this fake form below which
-  # mimics what the real sub_layout does, but without
-  # all of the overhead
-  %Q{
-  - import = NfgCsvImporter::Import.new
-  - form = NfgCsvImporter::Onboarding::ImportData::ImportTypeForm.new(import)
-  = form_for(form, url: '/path') do |f|
-    = yield f
-  }
-end
-
 RSpec.describe "nfg_csv_importer/onboarding/import_data/import_type.html.haml", type: :view do
   before do
     stub_template "nfg_csv_importer/onboarding/_sub_layout.html.haml" => fake_form
 
     view.stubs(:onboarder_presenter).returns(stub('Presenter', render_google_tag_manager: ''))
     view.stubs(:import_definitions).returns({ import_type => definition } )
-    form.stubs(:import_type).returns(nil)
     render
   end
 
   let(:import_type) { "example_import_type" }
   let(:form) { stub("form") }
+  let(:fake_form) do
+     # the sublayout needs to supply a form that is passed
+      # to its children. The text block below returns
+      # a fake version of the the sublayout which requires
+      # a lot more setup than is healthy for a simple view
+      # spec. So we return this fake form below which
+      # mimics what the real sub_layout does, but without
+      # all of the overhead
+        %Q{
+    - import = NfgCsvImporter::Import.new
+    - form = NfgCsvImporter::Onboarding::ImportData::ImportTypeForm.new(import)
+    = form_for(form, url: '/path') do |f|
+      = yield f
+      }
+  end
 
   context "when definition.import_title is nil" do
     let(:definition) { OpenStruct.new(import_title: nil, headline: "Example Headline") }
